6137b649546457345ce596f264bca6bcb1ddef58
EdT1200 <3lricloops@gmail.com>
2022-06-23 20:24:57 -0400

    player

---
+++ index.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector("canvas")
+const c = canvas.getContext("2d")
+class P1 {
+    constructor(){
+        this.position = {
+            
+        }
+    }
+
+}
+console.log(c)
